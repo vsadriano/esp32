7ca1b4b42515e6952979b6dd88a09fb429c91b0e
Adriano V. dos Santos <adriano.santos@serpro.gov.br>
2019-11-27 16:15:45 -0300

    Cayenne

---
+++ cayenne/cayenne.ino
@@ -0,0 +1,23 @@
+#include <CayenneMQTTESP32.h>
+
+char wifi_ssid[] = "";
+char wifi_pwd[] = "";
+char cayenne_user[] = "";
+char cayenne_pwd[] = "";
+char cayenne_id[] = "";
+
+CAYENNE_IN(0){
+  digitalWrite(LED_BUILTIN,getValue.asInt());
+  
+}
+
+void setup() {
+  Cayenne.begin(cayenne_user, cayenne_pwd, cayenne_id, wifi_ssid, wifi_pwd);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  Cayenne.loop();
+
+}
